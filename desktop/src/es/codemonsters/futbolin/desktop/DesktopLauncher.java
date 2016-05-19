833c5918d352b0e7f49a241ac3ba099e6ca73e5a
“Code <codemonsters@codemonsters.es>
2016-05-19 16:49:49 +0200

    Commit inicial

---
+++ desktop/src/es/codemonsters/futbolin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.codemonsters.futbolin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.codemonsters.futbolin.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
