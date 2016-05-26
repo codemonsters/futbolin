7812b6d3fd787b936e22d3451ffc419f9af8a632
“Code <codemonsters@codemonsters.es>
2016-05-26 16:53:48 +0200

    Cambiada resolución de la pantalla del proyecto Desktop

--- desktop/src/es/codemonsters/futbolin/desktop/DesktopLauncher.java
+++ desktop/src/es/codemonsters/futbolin/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import es.codemonsters.futbolin.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = MyGdxGame.TITLE;
+		config.width = 1024;
+		config.height = 576;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
