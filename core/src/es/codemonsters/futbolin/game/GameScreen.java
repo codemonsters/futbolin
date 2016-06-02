3363fc02ae12db0a03bfd6b87d33791a8b7ef531
12-trunks-12 <angel.miguelez.edu@gmail.com>
2016-06-02 17:11:36 +0200

    Código del juego

--- core/src/es/codemonsters/futbolin/game/GameScreen.java
+++ core/src/es/codemonsters/futbolin/game/GameScreen.java
@@ -3,6 +3,7 @@ package es.codemonsters.futbolin.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
@@ -12,37 +13,49 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import es.codemonsters.futbolin.MyGdxGame;
 
 public class GameScreen implements Screen {
 	
+	private FillViewport viewport;
+	private OrthographicCamera camera;
 	private MyGdxGame game;
 	private World world;
 	private Body body;
-	private Texture texture;
-	private Sprite sprite;
+	private Texture textureBola;
+	private Sprite spriteBola;
 	private Fixture fixture;
 	
-	private static final float WORLD_WIDTH = 1920;
-	private static final float WORLD_HEIGHT = 1080;
+	private static final float WORLD_WIDTH = 160;
+	private static final float WORLD_HEIGHT = 90;
 	
 	public GameScreen(final MyGdxGame game) {
-        this.game = game;
+        this.game = game; 
         
-        world = new World(new Vector2(0, -98f), true);
+        camera = new OrthographicCamera(); 
+        camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+        
+        viewport = new FillViewport(1024, 576, camera);
+        //viewport.setUnitsPerPixel(6.4f);
+        viewport.apply();
+        
+        world = new World(new Vector2(0, -0.5f), true);
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;   
         
-        texture = new Texture(Gdx.files.internal("ball_50x50.png"));
-        sprite = new Sprite(texture);
+        textureBola = new Texture(Gdx.files.internal("ball_50x50.png"));
+        spriteBola = new Sprite(textureBola);
         
-        bodyDef.position.set((WORLD_WIDTH+texture.getWidth())/2, (WORLD_HEIGHT+texture.getHeight())/2);
+        bodyDef.position.set((WORLD_WIDTH-textureBola.getWidth())/2, (WORLD_HEIGHT-textureBola.getHeight())/2);
         
         body = world.createBody(bodyDef);
         
         CircleShape shape = new CircleShape();
-        shape.setRadius(texture.getWidth()/2);
+        shape.setRadius(textureBola.getWidth()/2);
         
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
@@ -64,12 +77,14 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
-
-        sprite.setPosition(body.getPosition().x, body.getPosition().y);
+		camera.update();
+        spriteBola.setPosition(body.getPosition().x, body.getPosition().y);
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-        game.batch.draw(sprite, sprite.getX(), sprite.getY());
+        game.batch.draw(spriteBola, spriteBola.getX(), spriteBola.getY());
         game.batch.end();
 		
 	}
@@ -100,7 +115,7 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void dispose() {
-		texture.dispose();
+		textureBola.dispose();
 		world.dispose();
 		
 	}
