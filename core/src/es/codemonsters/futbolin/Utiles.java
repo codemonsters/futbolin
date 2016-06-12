53334f83aa456d595b5f065f4c5b27ed557bcdda
12-trunks-12 <anxomm@gmail.com>
2016-06-12 11:51:49 +0200

    Añadidas funciones de crear cuerpos y fixtures

--- core/src/es/codemonsters/futbolin/Utiles.java
+++ core/src/es/codemonsters/futbolin/Utiles.java
@@ -4,13 +4,37 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Shape;
 
 public class Utiles {
 	public static Texture crearTextura(int ancho, int alto, Color color) {
-		Pixmap pixmap = new Pixmap(ancho, alto, Format.RGBA8888);
-		pixmap.setColor(color);
-    	Texture pixmapTexture = new Texture(pixmap);
-    	pixmap.dispose();
+		Pixmap pixmap = new Pixmap(ancho, alto, Format.RGBA8888);  // Crea un pixmap
+		pixmap.setColor(color);  // Elige el color a utilizar para las siguientes operaciones
+		pixmap.fill();  // Rellenga el pixamp del color escogido
+		
+    	Texture pixmapTexture = new Texture(pixmap);  // Creacion de la textura a partir del pixmap
+    	pixmap.dispose();  // Elimina el pixmap
 		return pixmapTexture;
+	}
+	
+	public static Body crearCuerpo(World mundo, BodyType tipoCuerpo, float x, float y){
+		BodyDef bodyDef = new BodyDef();  // Guarda informacion del cuerpo (tipo, densidad, grados de rotacion, velocidad...)
+        bodyDef.type = tipoCuerpo;  // Tipo de cuerpo, el dinamico reacciona ante fuerzas, colisiones y otros eventos del mundo 
+        bodyDef.position.set(x, y);  // En los cuerpos el origen de coordenadas es el centro
+        Body body = mundo.createBody(bodyDef);  // Crea el cuerpo en el mundo que se indica
+        return body;     
+	}
+	
+	public static FixtureDef crearFixture(Shape formaCuerpo, float densidad, float friccion){ 
+        FixtureDef fixtureDef = new FixtureDef();  // Creacion de la forma fisica en el cuerpo con datos como la forma, la densidad, la friccion (en este caso no está puesta)...
+        fixtureDef.shape = formaCuerpo;
+        fixtureDef.density = densidad;
+        fixtureDef.friction = friccion;
+        return fixtureDef;
 	}
 }
