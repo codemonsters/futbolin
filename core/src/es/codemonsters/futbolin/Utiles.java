d82a99e89c9483cdba47c214a711928d3f06de70
12-trunks-12 <angel.miguelez.edu@gmail.com>
2016-06-16 17:47:00 +0200

    Cambiadas funciones de crear cuerpos

--- core/src/es/codemonsters/futbolin/Utiles.java
+++ core/src/es/codemonsters/futbolin/Utiles.java
@@ -4,8 +4,11 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
@@ -22,16 +25,42 @@ public class Utiles {
 		return pixmapTexture;
 	}
 	
-	public static Body crearCuerpo(World mundo, BodyType tipoCuerpo, float x, float y){
+	public static Body crearCuerpoCircular(World mundo, BodyType tipoCuerpo, float x, float y, float radio, float densidad, float friccion){
 		BodyDef bodyDef = new BodyDef();  // Guarda informacion del cuerpo (tipo, densidad, grados de rotacion, velocidad...)
         bodyDef.type = tipoCuerpo;  // Tipo de cuerpo, el dinamico reacciona ante fuerzas, colisiones y otros eventos del mundo 
-        bodyDef.position.set(x, y);  // En los cuerpos el origen de coordenadas es el centro
+        bodyDef.position.set(new Vector2(x, y));  // En los cuerpos el origen de coordenadas es el centro
         Body body = mundo.createBody(bodyDef);  // Crea el cuerpo en el mundo que se indica
+        
+        CircleShape shapeBody = new CircleShape();
+        shapeBody.setRadius(radio); 
+        
+        FixtureDef bodyFixture = crearFixture(shapeBody, densidad, friccion);
+        body.createFixture(bodyFixture);
+        
+        shapeBody.dispose();
+        
+        return body;     
+	}
+	
+	public static Body crearCuerpoRectangular(World mundo, BodyType tipoCuerpo, float x, float y, float ancho, float alto, float densidad, float friccion){
+		BodyDef bodyDef = new BodyDef();  // Guarda informacion del cuerpo (tipo, densidad, grados de rotacion, velocidad...)
+        bodyDef.type = tipoCuerpo;  // Tipo de cuerpo, el dinamico reacciona ante fuerzas, colisiones y otros eventos del mundo 
+        bodyDef.position.set(new Vector2(x, y));  // En los cuerpos el origen de coordenadas es el centro
+        Body body = mundo.createBody(bodyDef);  // Crea el cuerpo en el mundo que se indica
+        
+        PolygonShape shapeBody = new PolygonShape();
+        shapeBody.setAsBox(ancho/2, alto/2); 
+        
+        FixtureDef bodyFixture = crearFixture(shapeBody, densidad, friccion);
+        body.createFixture(bodyFixture);
+        
+        shapeBody.dispose();
+        
         return body;     
 	}
 	
 	public static FixtureDef crearFixture(Shape formaCuerpo, float densidad, float friccion){ 
-        FixtureDef fixtureDef = new FixtureDef();  // Creacion de la forma fisica en el cuerpo con datos como la forma, la densidad, la friccion (en este caso no está puesta)...
+        FixtureDef fixtureDef = new FixtureDef();  // Creacion de la forma fisica en el cuerpo con datos como la forma, la densidad, la friccion (en este caso no estï¿½ puesta)...
         fixtureDef.shape = formaCuerpo;
         fixtureDef.density = densidad;
         fixtureDef.friction = friccion;
